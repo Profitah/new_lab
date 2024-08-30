a09d34039c78afc66cd59c8aa3a4a6f39dd6eb79
이윤아 <101340482+Profitah@users.noreply.github.com>
2024-08-30 22:23:44 +0900

    lab 02 수정
    하기 싫다

--- Main_202012164.java
+++ Main_202012164.java
@@ -1,35 +1,68 @@
 import java.util.Scanner;
 
 public class Main_202012164 {
-
     public static void main(String[] args) {
-        System.out.println("lab0_1:이윤아"); 
-
-        Scanner scScanner = new Scanner(System.in);
+        System.out.println("lab0_2:이윤아");
 
         // 정수 개수 입력받기
-        int n = scScanner.nextInt();
+        Scanner skener = new Scanner(System.in);
+        int int_n = skener.nextInt();
 
-        // 크기가 n인 정수 배열 arrNumarray 생성
-        int[] arrNumarray = new int[n];
-        for (int idx = 0; idx < n; idx++) {
-            arrNumarray[idx] = scScanner.nextInt();
+        // n!를 구하여 출력
+        int factorialOutcome = factorial(int_n);
+        System.out.println(factorialOutcome);
+
+        // 1부터 n까지 정수를 출력
+        show(int_n);
+        System.out.println(); 
+
+        // 1부터 n까지 합을 구하여 출력_
+        int sumOutcome = sum(int_n);
+        System.out.println(sumOutcome);
+
+        // n부터 1까지 수를 출력
+        showReverse(int_n);
+        System.out.println(); 
+        
+
+        skener.close();
+    }
+
+    // n!을 구하여 리턴하는 메소드
+    private static int factorial(int n) {
+        if(n == 1) return 1;
+            else return n * factorial(n-1);
+    }
+
+
+    // 1 ~ n까지의 수를 합산
+    private static int sum(int n) {
+        if (n == 1)
+            return 1;
+        else
+            return n + sum(n - 1);
+    }
+
+    //// 1부터 n까지 출력하는 메소드
+    private static void show(int n) {
+        if (n > 0) {
+            show(n - 1); 
+            System.out.print(n + " ");
         }
+    }
 
-        // 크기가 10인 정수 배열 arrCount 생성
-        int[] arrCount = new int[10];
-        for (int idx = 0; idx < n; idx++) {
-            int indexValue = arrNumarray[idx];
-            if (indexValue >= 0 && indexValue <= 9) {
-                arrCount[indexValue]++;
-            }
+    // n 부터 1 까지의 수를 출력한다.
+    private static void showReverse(int n) {
+        if (n > 0) {
+            System.out.print(n + " ");
+            showReverse(n - 1); 
         }
+    }
 
-        // n개의 정수를 입력받아 양수만 출력
-        for (int i = 0; i < arrCount.length; i++) {
-            System.out.print(arrCount[i] + " ");  
-        }
-
-        scScanner.close(); 
+    private static int factorial(int n) {
+        if (n == 1)
+            return 1;
+        else
+            return n * factorial(n - 1);
     }
 }
