de99a84244e0c934c24c9313edd305fc310d09a0
이윤아 <101340482+Profitah@users.noreply.github.com>
2024-09-06 17:46:45 +0900

    lab_04

--- Main_202012164.java
+++ Main_202012164.java
@@ -1,68 +1,37 @@
 import java.util.Scanner;
 
 public class Main_202012164 {
+
     public static void main(String[] args) {
-        System.out.println("lab0_2:이윤아");
+        System.out.println("lab0_4: 이윤아");
 
-        // 정수 개수 입력받기
-        Scanner skener = new Scanner(System.in);
-        int int_n = skener.nextInt();
+        // 오름차순으로 정렬된 배열 array 초기화
+        int array[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
 
-        // n!를 구하여 출력
-        int factorialOutcome = factorial(int_n);
-        System.out.println(factorialOutcome);
+        // 대체할 원소를 입력받음
+        Scanner yoonahscanner = new Scanner(System.in);
+        int item = yoonahscanner.nextInt();
 
-        // 1부터 n까지 정수를 출력
-        show(int_n);
-        System.out.println(); 
+        /// 오름차순으로 정렬된 배열 array에서 마지막 원소를 item으로 대체
+        // ..
+        array[array.length - 1] = item;
 
-        // 1부터 n까지 합을 구하여 출력_
-        int sumOutcome = sum(int_n);
-        System.out.println(sumOutcome);
-
-        // n부터 1까지 수를 출력
-        showReverse(int_n);
-        System.out.println(); 
-        
-
-        skener.close();
-    }
-
-    // n!을 구하여 리턴하는 메소드
-    private static int factorial(int n) {
-        if(n == 1) return 1;
-            else return n * factorial(n-1);
-    }
-
-
-    // 1 ~ n까지의 수를 합산
-    private static int sum(int n) {
-        if (n == 1)
-            return 1;
-        else
-            return n + sum(n - 1);
-    }
-
-    //// 1부터 n까지 출력하는 메소드
-    private static void show(int n) {
-        if (n > 0) {
-            show(n - 1); 
-            System.out.print(n + " ");
+ 
+        for (int i = array.length - 1; i > 0; i--) {
+            if (array[i] < array[i - 1]) {
+                int temp = array[i];
+                array[i] = array[i - 1];
+                array[i - 1] = temp;
+            } else {
+                break;  
+            }
         }
-    }
 
-    // n 부터 1 까지의 수를 출력한다.
-    private static void showReverse(int n) {
-        if (n > 0) {
-            System.out.print(n + " ");
-            showReverse(n - 1); 
+        // 오름차순으로 정렬된 배열 내용 출력
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
-    }
 
-    private static int factorial(int n) {
-        if (n == 1)
-            return 1;
-        else
-            return n * factorial(n - 1);
+        yoonahscanner.close();
     }
 }
